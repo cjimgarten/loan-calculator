54d5198622d5be7e8dbfc2553f36fe722134cb00
Chris Imgarten <cjimgarten@gmail.com>
2017-08-23 19:36:24 -0500

    modify some stuff

--- src/main/java/com/cjimgarten/loancalculator/controller/LoanCalculatorController.java
+++ src/main/java/com/cjimgarten/loancalculator/controller/LoanCalculatorController.java
@@ -1,11 +1,13 @@
 package com.cjimgarten.loancalculator.controller;
 
 import com.cjimgarten.loancalculator.model.LoanDetails;
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.DecimalFormat;
 
 /**
  * Created by chris on 7/30/17.
@@ -24,17 +26,28 @@ public class LoanCalculatorController {
     @RequestMapping(value = "index", method = RequestMethod.GET)
     public String indexGET(Model model) {
         model.addAttribute("title", "Loan Calculator");
+
+        DecimalFormat df = new DecimalFormat("#,###.00");
+        if (loanDetails != null) {
+            model.addAttribute("amount",
+                    "$" + df.format(loanDetails.getAmount()));
+            model.addAttribute("monthlyPayment",
+                    "$" + df.format(loanDetails.getMonthlyPayment()));
+            model.addAttribute("totalPayment",
+                    "$" + df.format(loanDetails.getTotalPayment()));
+            model.addAttribute("totalInterest",
+                    "$" + df.format(loanDetails.getTotalInterest()));
+            model.addAttribute("annualPayment",
+                    "$" + df.format(loanDetails.getAnnualPayment()));
+        }
         return "index";
     }
 
     @RequestMapping(value = "index", method = RequestMethod.POST)
-    public String indexPOST(HttpServletRequest request) {
-        loanDetails = new LoanDetails(
-                Double.parseDouble(request.getParameter("amount")),
-                Double.parseDouble(request.getParameter("interestRate")),
-                Double.parseDouble(request.getParameter("term"))
-        );
-        loanDetails.calculatePayment();
+    public String indexPOST(@RequestParam double amount,
+                            @RequestParam double interestRate,
+                            @RequestParam double term) {
+        loanDetails = new LoanDetails(amount, interestRate, term);
         return "redirect:/calc/index";
     }
 }
--- src/main/java/com/cjimgarten/loancalculator/model/LoanDetails.java
+++ src/main/java/com/cjimgarten/loancalculator/model/LoanDetails.java
@@ -8,6 +8,10 @@ public class LoanDetails {
     private double amount;
     private double interestRate;
     private double term;
+    private double monthlyPayment;
+    private double totalPayment;
+    private double totalInterest;
+    private double annualPayment;
 
     public LoanDetails() {
 
@@ -17,6 +21,7 @@ public class LoanDetails {
         this.amount = amount;
         this.interestRate = interestRate;
         this.term = term;
+        refresh();
     }
 
     public double getAmount() {
@@ -25,7 +30,7 @@ public class LoanDetails {
 
     public void setAmount(double amount) {
         this.amount = amount;
-        calculatePayment();
+        refresh();
     }
 
     public double getInterestRate() {
@@ -34,7 +39,7 @@ public class LoanDetails {
 
     public void setInterestRate(double interestRate) {
         this.interestRate = interestRate;
-        calculatePayment();
+        refresh();
     }
 
     public double getTerm() {
@@ -43,13 +48,57 @@ public class LoanDetails {
 
     public void setTerm(double term) {
         this.term = term;
-        calculatePayment();
+        refresh();
     }
 
-    public void calculatePayment() {
-        // TODO calculate loan payment
-        System.out.println(amount);
-        System.out.println(interestRate);
-        System.out.println(term);
+    public double getMonthlyPayment() {
+        return this.monthlyPayment;
+    }
+
+    public double getTotalPayment() {
+        return this.totalPayment;
+    }
+
+    public double getTotalInterest() {
+        return this.totalInterest;
+    }
+
+    public double getAnnualPayment() {
+        return this.annualPayment;
+    }
+
+    public void refresh() {
+        calculateMonthlyPayment();
+        calculateTotalPayment();
+        calculateTotalInterest();
+        calculateAnnualPayment();
+    }
+
+    public void calculateMonthlyPayment() {
+
+        // variables needed to perform other calculations
+        double n = this.term * 12.0; // payments per year times number of years
+        double i = (this.interestRate * 0.01) / 12.0; // annual interest rate divided by number of payments per year
+
+        // calculate monthly loan payment
+        double dfPow = Math.pow((1+i), n); // part of the discount factor formula
+        double discountFactor = (dfPow - 1) / (i * dfPow); // not sure... just part of the formula
+        this.monthlyPayment = this.amount / discountFactor;
+    }
+
+    public void calculateTotalPayment() {
+        // TODO calculate the total payment
+        double n = this.term * 12.0; // payments per year times number of years
+        this.totalPayment = this.monthlyPayment * n;
+    }
+
+    public void calculateTotalInterest() {
+        // TODO calculate the total interest
+        this.totalInterest = this.totalPayment - this.amount;
+    }
+
+    public void calculateAnnualPayment() {
+        // TODO calculate the annual payment
+        this.annualPayment = this.monthlyPayment * 12;
     }
 }
